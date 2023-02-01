59b706ad2e9d56766095e3e9ef81c09256c48a29
jmcdlungren <j.mcd.lungren@gmail.com>
2023-02-01 10:10:27 -0700

    local storage done, working on color classes

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -6,39 +6,67 @@
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
   //
-  // var save = document.querySelector(".saveBtn");
-  // var description = document.querySelector(".description");
+
 
 
 
   $(".saveBtn").on("click", function saveTodo() {
-    // saveTodo.preventDefault();
+
     var description = $(this).siblings(".description").val();
-    var hour = $(this).siblings(".hour").val();
-    
-    // hour.push(description);
+    var hour = $(this).parent().attr("id");
+
+
 
     console.log(description);
-    
+
     localStorage.setItem(hour, description);
-    localStorage.getItem("hour");
+
   });
 
-  // need to use key (9AM)
+  for (i = 9; i <= 17; i++) {
+    var data = localStorage.getItem("hour-" + i)
+    $("#hour-" + i).children(".description").val(data)
 
-  // submit.addEventListener("click", function(event) {
-  //   event.preventDefault();
-  //   var initials = initialsEl.value;
-  //   var userScore = finalScore;
-  //   console.log(initials, userScore);
-  //   var userData = {
-  //       initials: initials,
-  //       score: userScore,
-  //   };
 
-  //   scoreBoard.push(userData);
-  //   console.log(scoreBoard);
-  //   localStorage.setItem("scoreBoard", JSON.stringify(scoreBoard));
+    // var time = document.querySelector(".time-block");
+
+    // var hour = $(this).children(".hour").val();
+
+
+    var rightNow = dayjs().format('hA');
+    var hour = $(time).children(".hour").val();
+    if (hour.textContent === rightNow) {
+      $(this).classlist.add("present");
+    } else {
+      if (hour.textContent < rightNow) {
+        $(this).classList.add("past")
+      } else {
+        if (hour.textContent > rightNow) {
+          $(this).classList.add("future")
+        }
+      }
+    };
+
+
+
+    // for (var i = 0; i < time.length; i++) {
+
+    //   if (hour.textContent === rightNow) {
+    //     time[i].classList.add("present");
+    //   } else {
+    //     if (hour.textContent < rightNow) {
+    //       time[i].classList.add("past")
+    //     } else {
+    //       if (hour.textContent > rightNow) {
+    //         time[i].classList.add("future")
+    //       }
+    //     }
+    //   };
+
+    // }
+  }
+
+
 
 
 
@@ -46,11 +74,9 @@ $(function () {
 
   // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
   //
-  var time = document.querySelector(".time-block");
-  // var seven = document.querySelector("#hour-7");
-  // var eight = document.querySelector("#hour-8");
-  // var eleven = document.querySelector("#hour-9");
-  var hour = document.querySelector(".hour");
+  // var time = document.querySelector(".time-block");
+
+  // var hour = $(time).children(".hour").val();
   var rightNow = dayjs().format('hA');
   var today = dayjs().format('dddd, MMMM Do')
   var currentDay = document.querySelector("#currentDay");
@@ -59,14 +85,15 @@ $(function () {
     currentDay.textContent = today;
   }
 
-  // for (var i = 0; i < time.length; i++ && var i = 0; i < hour.length) {
-  //   if (hour[i].textContent === rightNow) {
+  // for (var i = 0; i < time.length; i++) {
+
+  //   if (hour.textContent === rightNow) {
   //     time[i].classList.add("present");
   //   } else {
-  //     if (hour[i].textContent < rightNow) {
+  //     if (hour.textContent < rightNow) {
   //       time[i].classList.add("past")
   //     } else {
-  //       if (hour[i].textContent > rightNow) {
+  //       if (hour.textContent > rightNow) {
   //         time[i].classList.add("future")
   //       }
   //     }
