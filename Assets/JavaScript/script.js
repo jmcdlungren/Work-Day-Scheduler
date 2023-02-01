3db068d1c6834be129eaf21cdaf7573496101161
jmcdlungren <j.mcd.lungren@gmail.com>
2023-02-01 10:13:22 -0700

    stuck on variables for time / color section

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -6,67 +6,31 @@
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
   //
-
+  
 
 
 
   $(".saveBtn").on("click", function saveTodo() {
-
+    
     var description = $(this).siblings(".description").val();
     var hour = $(this).parent().attr("id");
-
-
+    
+    
 
     console.log(description);
-
+    
     localStorage.setItem(hour, description);
-
+    
   });
 
   for (i = 9; i <= 17; i++) {
-    var data = localStorage.getItem("hour-" + i)
-    $("#hour-" + i).children(".description").val(data)
+    var data = localStorage.getItem("hour-"+i)
+    $("#hour-"+i).children(".description").val(data)
 
-
-    // var time = document.querySelector(".time-block");
-
-    // var hour = $(this).children(".hour").val();
-
-
-    var rightNow = dayjs().format('hA');
-    var hour = $(time).children(".hour").val();
-    if (hour.textContent === rightNow) {
-      $(this).classlist.add("present");
-    } else {
-      if (hour.textContent < rightNow) {
-        $(this).classList.add("past")
-      } else {
-        if (hour.textContent > rightNow) {
-          $(this).classList.add("future")
-        }
-      }
-    };
-
-
-
-    // for (var i = 0; i < time.length; i++) {
-
-    //   if (hour.textContent === rightNow) {
-    //     time[i].classList.add("present");
-    //   } else {
-    //     if (hour.textContent < rightNow) {
-    //       time[i].classList.add("past")
-    //     } else {
-    //       if (hour.textContent > rightNow) {
-    //         time[i].classList.add("future")
-    //       }
-    //     }
-    //   };
-
-    // }
+    
   }
 
-
+ 
 
 
 
@@ -74,8 +38,9 @@ $(function () {
 
   // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
   //
-  // var time = document.querySelector(".time-block");
-
+  var time = document.querySelector(".time-block");
+  
+  var hour = document.querySelector(".hour");
   // var hour = $(time).children(".hour").val();
   var rightNow = dayjs().format('hA');
   var today = dayjs().format('dddd, MMMM Do')
@@ -86,18 +51,18 @@ $(function () {
   }
 
   // for (var i = 0; i < time.length; i++) {
-
-  //   if (hour.textContent === rightNow) {
-  //     time[i].classList.add("present");
-  //   } else {
-  //     if (hour.textContent < rightNow) {
-  //       time[i].classList.add("past")
-  //     } else {
-  //       if (hour.textContent > rightNow) {
-  //         time[i].classList.add("future")
-  //       }
-  //     }
-  //   };
+    
+    if (hour.textContent === rightNow) {
+      time.classList.add("present");
+    } else {
+      if (hour.textContent < rightNow) {
+        time.classList.add("past")
+      } else {
+        if (hour.textContent > rightNow) {
+          time.classList.add("future")
+        }
+      }
+    };
 
   // }
 
