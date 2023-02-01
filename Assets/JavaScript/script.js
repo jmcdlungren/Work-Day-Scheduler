1672aab9b545dcccfda827872f1a538815d48409
jmcdlungren <j.mcd.lungren@gmail.com>
2023-02-01 09:33:45 -0700

    working on local storage

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -6,19 +6,25 @@
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
   //
-  var save = document.querySelector(".saveBtn");
-  var description = document.querySelector(".description");
+  // var save = document.querySelector(".saveBtn");
+  // var description = document.querySelector(".description");
 
 
 
-  save.addEventListener("click", function saveTodo() {
+  $(".saveBtn").on("click", function saveTodo() {
     // saveTodo.preventDefault();
-    var toDoItems = description.value;
-
-    console.log(toDoItems);
+    var description = $(this).siblings(".description").val();
+    var hour = $(this).siblings(".hour").val();
     
+    // hour.push(description);
+
+    console.log(description);
+    
+    localStorage.setItem(hour, description);
+    localStorage.getItem("hour");
   });
 
+  // need to use key (9AM)
 
   // submit.addEventListener("click", function(event) {
   //   event.preventDefault();
@@ -53,20 +59,20 @@ $(function () {
     currentDay.textContent = today;
   }
 
-  for (var i = 0; i < time.length; i++) {
-    if (hour.textContent === rightNow) {
-      time[i].classList.add("present");
-    } else {
-      if (hour.textContent < rightNow) {
-        time[i].classList.add("past")
-      } else {
-        if (hour.textContent > rightNow) {
-          time[i].classList.add("future")
-        }
-      }
-    };
+  // for (var i = 0; i < time.length; i++ && var i = 0; i < hour.length) {
+  //   if (hour[i].textContent === rightNow) {
+  //     time[i].classList.add("present");
+  //   } else {
+  //     if (hour[i].textContent < rightNow) {
+  //       time[i].classList.add("past")
+  //     } else {
+  //       if (hour[i].textContent > rightNow) {
+  //         time[i].classList.add("future")
+  //       }
+  //     }
+  //   };
 
-  }
+  // }
 
 
 
